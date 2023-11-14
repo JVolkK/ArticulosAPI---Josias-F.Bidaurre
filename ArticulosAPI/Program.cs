5fe5cd00734e4e24e844c8d074fe20f106573b41
Administrator <Administrator@WIN-8GKPEUHM58S>
2023-11-14 17:20:42 -0300

    Add project files.

---
+++ ArticulosAPI/Program.cs
@@ -0,0 +1,38 @@
+using ArticulosAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+IMapper mapper = MappingConfiguration.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IArticuloRepositorio, ArticuloRepositorio>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ArticulosAPI/Repositorio/ArticuloRepositorio.cs
@@ -0,0 +1,55 @@
+﻿using ArticulosAPI.Data;
+using ArticulosAPI.Modelos;
+using ArticulosAPI.Dto;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+public class ArticuloRepositorio : IArticuloRepositorio
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public ArticuloRepositorio(ApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ArticuloDto>> ObtenerArticulos()
+    {
+        var articulos = await _context.Articulos.ToListAsync();
+        return articulos.Select(a => _mapper.Map<ArticuloDto>(a));
+    }
+
+    public async Task<ArticuloDto> ObtenerArticuloPorId(int id)
+    {
+        var articulo = await _context.Articulos.FirstOrDefaultAsync(a => a.Id == id);
+        return _mapper.Map<ArticuloDto>(articulo);
+    }
+
+    public async Task AgregarArticulo(ArticuloDto articuloDto)
+    {
+        var articulo = _mapper.Map<Articulo>(articuloDto);
+
+        _context.Articulos.Add(articulo);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task ActualizarArticulo(int id, ArticuloDto articuloDto)
+    {
+        var articulo = await _context.Articulos.FirstOrDefaultAsync(a => a.Id == id);
+
+        if (articulo != null)
+        {
+            _mapper.Map(articuloDto, articulo);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task EliminarArticulo(int id)
+    {
+        var articulo = await _context.Articulos.FirstOrDefaultAsync(a => a.Id == id);
+        _context.Articulos.Remove(articulo);
+        await _context.SaveChangesAsync();
+    }
+}
